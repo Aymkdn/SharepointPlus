21cb26e60dd336a06c379a50619040c642a25143
Aymeric <Aymkdn@users.noreply.github.com>
2019-03-25 12:37:55 +0100

    v5.2

--- sharepointplus-5.2.js
+++ sharepointplus-5.2.js
@@ -1,5 +1,5 @@
 ﻿/*!
- * SharepointPlus v5.1
+ * SharepointPlus v5.2
  * Copyright 2018, Aymeric (@aymkdn)
  * Contact: http://kodono.info
  * Documentation: http://aymkdn.github.com/SharepointPlus/
@@ -158,7 +158,7 @@ var _SP_JSON_ACCEPT="verbose"; // other options are "minimalmetadata" and "nomet
 
       @return {String} The current SharepointPlus version
     */
-    getVersion:function() { return "5.1" },
+    getVersion:function() { return "5.2" },
     /**
      * @ignore
      * @name $SP()._promise
